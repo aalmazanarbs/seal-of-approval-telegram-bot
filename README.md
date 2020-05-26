8de3bc530ca652ca99d88b509c23e3fcb04c8b23
Alejandro Almazán Cabo <a.almazan.ext@orangebank.es>
2020-05-26 22:16:59 +0200

    Review run configuration

--- README.md
+++ README.md
@@ -35,5 +35,5 @@ $ sh run clean
 
 for restart just
 ```bash
-$ ./run.sh clean
+$ ./run.sh
 ```
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { TelegramBot } from 'nest-telegram';
-import { SealOfApprovalTelegramBotModule } from './seal-of-approval-telegram-bot.module';
+import { AppModule } from './app.module';
 import { resolveAddress } from './network/address-resolver';
 
 async function bootstrap() {
     const isProduction = process.env.NODE_ENV === 'production';
 
     const app = await NestFactory.create<NestFastifyApplication>(
-        SealOfApprovalTelegramBotModule,
+        AppModule,
         new FastifyAdapter({ logger: true })
     );
 
     const bot = app.get(TelegramBot);
 
     if (isProduction) {
-        app.use(bot.getMiddleware('hook-path', { source: process.env.BOT_CERTITICATE_PATH }));
+        const secretPath = process.env.BOT_SECRET_WEBHOOK_PATH;
+        if (secretPath === undefined || secretPath === null || secretPath === '') {
+            throw new Error('Not set environment variable BOT_SECRET_WEBHOOK_PATH');
+        }
+        app.use(bot.getMiddleware(secretPath, { source: process.env.BOT_CERTITICATE_PATH }));
     }
 
-    const module = app.get(SealOfApprovalTelegramBotModule);
+    const module = app.get(AppModule);
 
     bot.init(module.getModuleRef(), !isProduction);
 
-    await app.listen(+process.env.APP_PORT || 3000, resolveAddress(isProduction));
+    await app.listen(+process.env.APP_PORT ?? 3000, resolveAddress(isProduction));
 }
 
 bootstrap();
